0ba4699e64da9f678402e04edb785cf63f85ac49
ARIAS <farias@laserargentina.local>
2021-10-21 13:58:19 -0300

    ejoramos carrito

--- js/carrito.js
+++ js/carrito.js
@@ -7,6 +7,7 @@ const imprimirProducto = () => {
         <aritcle class="col-lg-3 py-3 col-md-6">
         <h3 class="productos3D__tittle--whiteSmoke text-center">${obj.nombre}</h3>
         <img src="img/${obj.nombre}.jpg" alt="Modelo 3D de placas antihumedad burbuja" class="w-100 h-75">
+        <h5 class="card-title text-center"><span class="badge rounded-pill bg-success">usted compro ${obj.metros} ${obj.unidad} de ${obj.nombre} </span></h5>
         `;
     }); 
 }
--- js/index.js
+++ js/index.js
@@ -1,52 +1,63 @@
 class Productos {
-    constructor (nombre,modelo,precio){
+    constructor (nombre,modelo,precio,metros,unidad){
         this.nombre = nombre,
         this.modelo = modelo,
-        this.precio = precio
+        this.precio = precio,
+        this.metros = metros,
+        this.unidad = unidad
     }
 }
 
 let productosCargados = [];
-var producto = new Productos("BURBUJA","mod-3D",700)
+var producto = new Productos("BURBUJA","mod-3D",700,0,"metros")
 productosCargados.push(producto)
-var producto = new Productos("TECNO","mod-3D",700)
+var producto = new Productos("TECNO","mod-3D",700,0,"metros")
 productosCargados.push(producto)
-var producto = new Productos("FLOR","mod-3D",700)
+var producto = new Productos("FLOR","mod-3D",700,0,"metros")
 productosCargados.push(producto)
-var producto = new Productos("PIEDRA","mod-rus",700)
+var producto = new Productos("PIEDRA","mod-rus",800,0,"metros")
 productosCargados.push(producto)
-var producto = new Productos("MAYA","mod-rus",700)
+var producto = new Productos("MAYA","mod-rus",700,0,"metros")
 productosCargados.push(producto)
-var producto = new Productos("PIZARRA","mod-rus",700)
+var producto = new Productos("PIZARRA","mod-rus",850,0,"metros")
 productosCargados.push(producto)
-var producto = new Productos("LORETO","mod-rus",700)
+var producto = new Productos("LORETO","mod-rus",850,0,"metros")
 productosCargados.push(producto)
-var producto = new Productos("PEGAMENTO ZIKA","prod-adicionales",700)
+var producto = new Productos("PEGAMENTO ZIKA","prod-adicionales",700,0,"bolsa/bolsas")
 productosCargados.push(producto)
-var producto = new Productos("MASILLA","prod-adicionales",700)
+var producto = new Productos("MASILLA","prod-adicionales",700,0,"tacho/tachos")
 productosCargados.push(producto)
-var producto = new Productos("YESO SELENITA","prod-adicionales",700)
+var producto = new Productos("YESO SELENITA","prod-adicionales",700,0,"bolsa/bolsas")
 productosCargados.push(producto)
-var producto = new Productos("PINTURA LATEX","prod-adicionales",700)
+var producto = new Productos("PINTURA LATEX","prod-adicionales",700,0,"lata/latas")
 productosCargados.push(producto)
 
 const imprimirProducto = () => {
     productosCargados.forEach(obj =>{
         let index = productosCargados.indexOf(obj)
         idSeleccionado = obj.modelo
-        document.getElementById(idSeleccionado).innerHTML+=`
+        document.getElementById(idSeleccionado).innerHTML += `
         <aritcle class="col-lg-3 py-3 col-md-6">
-        <h3 class="productos3D__tittle--whiteSmoke text-center">${obj.nombre}</h3>
+        <h3 class="productos3D__tittle--whiteSmoke text-center">${obj.nombre}  <span class="badge rounded-pill bg-success"> $${obj.precio}</span></h3>
         <img src="img/${obj.nombre}.jpg" alt="Modelo 3D de placas antihumedad burbuja" class="w-100 h-75">
         <div class="w-100">
-            <input type="number" placeholder="Cantidad" class="w-100 mt-3" id"cant">
-            <button id="bg-perzonalizado" type="button" class="btn btn-info w-100 mt-1" onclick="ComprarProducto(${index})">Comprar</button>
+            <form action="" id="miForm${index}">
+                <input type="number" placeholder="Cantidad" class="w-100 mt-3" id="cant${index}">
+                <button id="bg-perzonalizado" type="button" class="btn btn-info w-100 mt-1" onclick="ComprarProducto(${index})">Comprar</button>
+            </form>
         </div>
         `;
     }); 
 }
 
 const ComprarProducto = (index) =>{
+    let metros = document.getElementById(`cant${index}`).value
+    productosCargados[index].metros = metros
+    
+    if(metros == 0){
+        alert("No puede comprar un producto sin seleccionar la cantidad");
+    }
+    else{
         let carrito;
         if (localStorage.getItem("carrito") == null ) {
             carrito = []
@@ -56,10 +67,13 @@ const ComprarProducto = (index) =>{
         }
         carrito.push(productosCargados[index])
         localStorage.setItem("carrito", JSON.stringify(carrito))
+        document.getElementById(`miForm${index}`).reset();
+        alert(`Acabas de agregar al carrito ${metros} de ${productosCargados[index].nombre}`)
+    }
+        
     
     
 }
-
 imprimirProducto()
 
 
