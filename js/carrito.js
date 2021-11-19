f43792d0b24fa045388402ed1f4e8b47d28d33f2
franco arias <francosamuel1999@gmail.com>
2021-11-19 02:12:48 -0300

    quitamos innerhtml

--- js/carrito.js
+++ js/carrito.js
@@ -1,13 +1,67 @@
+
+// Declaramos el carrito en el localstorage
 carrito = JSON.parse(localStorage.getItem("carrito"))
-console.log(carrito);
+
+// Declaramos algunas variables utiles
 let precioTotal = 0;
 let totalCompra;
 
+
+/* Declaramos las principales funciones */ 
+
+// Funcion para imprimir un producto desde el carrito
 const imprimirProducto = () => {
     carrito.forEach(obj =>{
         let totalCompra = 0;
         totalCompra = obj.metros * obj.precio 
         precioTotal = precioTotal + totalCompra
+
+        /* Reciclamos codigo */
+
+        // Creamo la etiqueta principal, en este caso un article
+        const article = document.createElement("article")
+        article.setAttribute("class","col-lg-3 py-3 col-md-6")
+
+        // Creamos un h3 y luego lo pusheamos en la etiqueta aticle
+        const h3 = document.createElement("h3")
+        h3.setAttribute("class", "productos3D__tittle--whiteSmoke text-center titu")
+        let contentH3 = obj.nombre
+        h3.textContent = contentH3
+        article.appendChild(h3)
+
+        // Creamos un img y luego lo pusheamos en la etiqueta aticle
+        const img = document.createElement("img")
+        img.setAttribute("src",`img/${obj.nombre}.jpg`)
+        img.setAttribute("class", "w-100 h-75")
+        article.appendChild(img)
+        
+        //Creamos un h5 para mostrar el resumen de lo comprado
+        const h5Primero = document.createElement("h5")
+        h5Primero.setAttribute("class","card-title text-center")
+        h5Primero.textContent = ""
+        // Creamos un span que ira dentro del h5 para resaltar
+        const span = document.createElement("span")
+        span.setAttribute("class","badge rounded-pill bg-success")
+        let contentSpan = `compro ${obj.metros} ${obj.unidad} de ${obj.nombre}`
+        span.textContent = contentSpan
+        h5Primero.appendChild(span)
+        article.appendChild(h5Primero)
+        // Creamos otro h5 para mostrar el valor de esa compra en particular
+        const h5Segundo = document.createElement("h5")
+        h5Segundo.setAttribute("class","card-title text-center")
+        h5Segundo.textContent = ""
+        // Creamos otro span para resaltar
+
+        const spanSegundo = document.createElement("span")
+        spanSegundo.setAttribute("class","badge rounded-pill bg-success")
+        let contentSpan2 = totalCompra
+        spanSegundo.textContent ="$ " + contentSpan2
+        h5Segundo.appendChild(spanSegundo)
+        article.appendChild(h5Segundo)
+
+        document.getElementById("carrito").appendChild(article)
+
+        /*
         document.getElementById("carrito").innerHTML+=`
         <aritcle class="col-lg-3 py-3 col-md-6 carrito">
         <h3 class="productos3D__tittle--whiteSmoke text-center">${obj.nombre}</h3>
@@ -15,7 +69,7 @@ const imprimirProducto = () => {
         <h5 class="card-title text-center"><span class="badge rounded-pill bg-success">compro ${obj.metros} ${obj.unidad} de ${obj.nombre} </span></h5>
         <h5 class="card-title text-center"><span class="badge rounded-pill bg-success">$${totalCompra}</span></h5>
         <article/>
-        `;
+        `;*/
         
     }); 
 }
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+/* Creamos el objeto principal */
 class Productos {
     constructor (nombre,modelo,precio,metros,unidad){
         this.nombre = nombre,
@@ -9,6 +9,7 @@ class Productos {
     }
 }
 
+/* Cargamos a mano los productos */
 let productosCargados = [];
 var producto = new Productos("BURBUJA","mod-3D",700,0,"metros")
 productosCargados.push(producto)
@@ -32,11 +33,115 @@ var producto = new Productos("YESO SELENITA","prod-adicionales",700,0,"bolsa/bol
 productosCargados.push(producto)
 var producto = new Productos("PINTURA LATEX","prod-adicionales",700,0,"lata/latas")
 productosCargados.push(producto)
+
+
+
+
+const imprimirProducto = () => {
+    let i = 0;
+    productosCargados.forEach(obj =>{
+        if (localStorage.getItem("productos") == null ) {
+            productos2 = []
+        }
+        else{
+            productos2 = JSON.parse(localStorage.getItem("productos"))
+        }
+        productos2.push(productosCargados[i])
+        console.log(productosCargados[i])
+        localStorage.setItem("productos", JSON.stringify(productos2))
+        let index = productosCargados.indexOf(obj)
+        i = i + 1
+        // Creamos un ID unico para cada modelo
+        idSeleccionado = obj.modelo
+
+
+        /* Creamos la estructura que posteriormente vamos a iyectar en el HTML */
+        // Creamo la etiqueta principal, en este caso un article
+        const article = document.createElement("article")
+        article.setAttribute("class","col-lg-3 py-3 col-md-6")
+
+        // Creamos un h3 y luego lo pusheamos en la etiqueta aticle
+        const h3 = document.createElement("h3")
+        h3.setAttribute("class", "productos3D__tittle--whiteSmoke text-center titu")
+        let contentH3 = obj.nombre
+        h3.textContent = contentH3
+        const span = document.createElement("span")
+        span.setAttribute("class","badge rounded-pill bg-success precio")
+        span.setAttribute("id",`${index}`)
+        let contentSpan = "$" + obj.precio
+        span.textContent = contentSpan
+        h3.appendChild(span)
+        article.appendChild(h3)
+
+        // Creamos un img y luego lo pusheamos en la etiqueta aticle
+        const img = document.createElement("img")
+        img.setAttribute("src",`img/${obj.nombre}.jpg`)
+        img.setAttribute("class", "w-100 h-75")
+        article.appendChild(img)
+
+        // Creamos un div y luego de crear un form lo pusheamos a la etiqueta aticle
+        const div = document.createElement("div")
+        div.setAttribute("class","w-100")
+
+        // Creamos un form y luego de crear un input y un button lo pusheamos a la etiqueta div
+        const form = document.createElement("form")
+        form.setAttribute("id",`miForm${index}`)
+
+        // Creamos un input y luego lo pusheamos a la etiqueta form
+        const input = document.createElement("input")
+        input.setAttribute("type","number")
+        input.setAttribute("placeholder","Cantidad")
+        input.setAttribute("class","w-100 mt-3")
+        input.setAttribute("id",`cant${index}`)
+        form.appendChild(input)
+
+        // Creamos un buttom y lo pusheamos a la etiqueta form
+        const button = document.createElement("button")
+        button.setAttribute("id","bg-perzonalizado")
+        button.setAttribute("class","btn btn-info w-100 mt-1")
+        button.setAttribute("type","button")
+        button.setAttribute("onclick",`ComprarProducto(${index})`)
+        button.textContent = "Comprar"
+        form.appendChild(button)
+        
+        div.appendChild(form)
+        article.appendChild(div)
+        document.getElementById(idSeleccionado).appendChild(article)
+    }); 
+}
+
+/* Declaramos las funciones principales */
+
+// Funcion para redondear a 2 decimales
 function round(num) {
     var m = Number((Math.abs(num) * 100).toPrecision(15));
     return Math.round(m) / 100 * Math.sign(num);
 }
-$("#btn-dolar").click(()=> {
+
+// Funcion para comprar un producto y agregarlo al carrito
+const ComprarProducto = (index) =>{
+    let metros = document.getElementById(`cant${index}`).value
+    productosCargados[index].metros = metros
+    
+    if(metros == 0){
+        alert("No puede comprar un producto sin seleccionar la cantidad");
+    }
+    else{
+        let carrito;
+        if (localStorage.getItem("carrito") == null ) {
+            carrito = []
+        }
+        else{
+            carrito = JSON.parse(localStorage.getItem("carrito"))
+        }
+        carrito.push(productosCargados[index])
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+        document.getElementById(`miForm${index}`).reset();
+        alert(`Acabas de agregar al carrito ${metros} ${productosCargados[index].unidad} de ${productosCargados[index].nombre}`)  
+    }    
+}
+// Funcion para cambiar el precio de dolar a pesos o viceversa
+const cambiarPrecio = () => {
     var dolar = 0
     const url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
     $.get(url, (data, estado) => {
@@ -70,71 +175,13 @@ $("#btn-dolar").click(()=> {
             })
         }
     })
-       
-   
-})
 
-
-
-
-
-const imprimirProducto = () => {
-    let i = 0;
-    productosCargados.forEach(obj =>{
-        if (localStorage.getItem("productos") == null ) {
-            productos2 = []
-        }
-        else{
-            productos2 = JSON.parse(localStorage.getItem("productos"))
-        }
-        productos2.push(productosCargados[i])
-        console.log(productosCargados[i])
-        localStorage.setItem("productos", JSON.stringify(productos2))
-        let index = productosCargados.indexOf(obj)
-        i = i + 1
-        console.log(i)
-        idSeleccionado = obj.modelo
-        document.getElementById(idSeleccionado).innerHTML += `
-        <aritcle class="col-lg-3 py-3 col-md-6 ">
-        <h3 class="productos3D__tittle--whiteSmoke text-center titu">${obj.nombre}  <span class="badge rounded-pill bg-success precio" id="${index}" >$${obj.precio}</span></h3>
-        <img src="img/${obj.nombre}.jpg" alt="Modelo 3D de placas antihumedad burbuja" class="w-100 h-75 ">
-        <div class="w-100">
-            <form action="" id="miForm${index}">
-                <input type="number" placeholder="Cantidad" class="w-100 mt-3" id="cant${index}">
-                <button id="bg-perzonalizado" type="button" class="btn btn-info w-100 mt-1 " onclick="ComprarProducto(${index})">Comprar</button>
-            </form>
-        </div>
-        <article/>
-        `;
-    }); 
 }
 
-const ComprarProducto = (index) =>{
-    let metros = document.getElementById(`cant${index}`).value
-    productosCargados[index].metros = metros
-    
-    if(metros == 0){
-        alert("No puede comprar un producto sin seleccionar la cantidad");
-    }
-    else{
-        let carrito;
-        if (localStorage.getItem("carrito") == null ) {
-            carrito = []
-        }
-        else{
-            carrito = JSON.parse(localStorage.getItem("carrito"))
-        }
-        carrito.push(productosCargados[index])
-        localStorage.setItem("carrito", JSON.stringify(carrito))
-        document.getElementById(`miForm${index}`).reset();
-        alert(`Acabas de agregar al carrito ${metros} ${productosCargados[index].unidad} de ${productosCargados[index].nombre}`)  
-    }   
-    
-    
-}
+/* Programa */
+
 imprimirProducto()
-
-console.log("hola hola");
+$("#btn-dolar").click(cambiarPrecio)
 
 
 
